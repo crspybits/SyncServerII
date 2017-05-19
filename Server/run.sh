bc4de5bd554a56dc1a5a476c33fec540f05a0100
Christopher Prince <chris@SpasticMuffin.biz>
2017-05-18 20:56:33 -0600

    Modified run.sh script.

--- Server/run.sh
+++ Server/run.sh
@@ -3,7 +3,7 @@
 # Build and run the server from the command line
 # Optionally, you can give a single argument: build or test. The default is build, which will also run the server after building.
 
-buildLocation=/tmp/.build-server
+buildLocation=~/builds/.build-server
 
 CMD=$1
 if [ "empty${CMD}" == "empty" ] ; then
@@ -14,7 +14,6 @@ if [ $? == 0 ] && [ "${CMD}" == "test" ] ; then
 	# Some test cases expect `Cat.jpg` in /tmp
 	cp Resources/Cat.jpg /tmp
 fi
-
 
 # use --verbose flag to show more output
 swift "${CMD}" -Xswiftc -DDEBUG -Xswiftc -DSERVER --build-path "${buildLocation}"
