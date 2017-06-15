2a1ef6b50ef6c59d7cd7582d887457620939d55c
Christopher Prince <chris@SpasticMuffin.biz>
2017-06-15 16:52:39 -0600

    Bumped up timeout on test case.

--- iOS/Example/Tests/ServerAPI_DownloadFile.swift
+++ iOS/Example/Tests/ServerAPI_DownloadFile.swift
@@ -71,6 +71,6 @@ class ServerAPI_DownloadFile: TestCase {
             expectation2.fulfill()
         }
         
-        waitForExpectations(timeout: 40.0, handler: nil)
+        waitForExpectations(timeout: 120.0, handler: nil)
     }
 }
