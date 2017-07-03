bc5268ab035cb3e3e6aa582435675deda31bfe7a
Christopher Prince <chris@SpasticMuffin.biz>
2017-07-02 20:00:24 -0600

    Fixed issue with a test case.

--- iOS/Example/Tests/HealthCheck.swift
+++ iOS/Example/Tests/HealthCheck.swift
@@ -13,7 +13,7 @@ class ServerAPI_HealthCheck: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        ServerNetworking.session.authenticationDelegate = nil
     }
     
     override func tearDown() {
