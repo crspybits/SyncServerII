df3961abaa728695f71587ea80517aaa3d1c90b6
Christopher Prince <chris@SpasticMuffin.biz>
2018-03-03 20:55:37 -0700

    Edits to migration for last relaese.

--- Migrations/2.sql
+++ Migrations/2.sql
@@ -1,5 +1,7 @@
-#USE SyncServer_SharedImages;
-#UPDATE User SET cloudFolderName = "SharedImages.Folder" WHERE userType = "owning";
+USE SyncServer_SharedImages;
+ALTER TABLE User ADD cloudFolderName VARCHAR(256);
+UPDATE User SET cloudFolderName = "SharedImages.Folder" WHERE userType = "owning";
 
-USE syncserver;
-UPDATE User SET cloudFolderName = "Staging.SharedImages.Folder" WHERE userType = "owning";
+# USE SharedImages_Staging;
+# ALTER TABLE User ADD cloudFolderName VARCHAR(256);
+# UPDATE User SET cloudFolderName = "Staging.SharedImages.Folder" WHERE userType = "owning";
