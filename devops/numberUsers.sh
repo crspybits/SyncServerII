6300af511b524011e6971f6bf2aa8f098c975474
Christopher Prince <chris@SpasticMuffin.biz>
2019-01-07 19:18:46 -0700

    Added script to count number of users on system.

---
+++ devops/numberUsers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Usage: numberUsers.sh <Server.json>
+
+# e.g., ./numberUsers.sh ~/Desktop/Apps/SyncServerII/Private/Server.json.aws.app.bundles/SharedImages-production.json
+
+MY_SQL_JSON=$1
+DATABASE=`jq -r '.["mySQL.database"]' < ${MY_SQL_JSON}`
+USER=`jq -r '.["mySQL.user"]' < ${MY_SQL_JSON}`
+PASSWORD=`jq -r '.["mySQL.password"]' < ${MY_SQL_JSON}`
+HOST=`jq -r '.["mySQL.host"]' < ${MY_SQL_JSON}`
+
+SCRIPT="select count(*) from User;"
+
+echo -n "Number of users: "
+mysql -P 3306 --password="$PASSWORD" --user="$USER" --host="$HOST" --database="$DATABASE" -Bse "$SCRIPT" 2>&1 | grep -v "Using a password"
