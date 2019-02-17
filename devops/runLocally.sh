9773ae79886839f4fcdb55ef1e92863da6f8858b
Christopher Prince <chris@SpasticMuffin.biz>
2019-02-17 01:32:42 -0700

    Making migration script.

--- devops/runLocally.sh
+++ devops/runLocally.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Usage: runLocally.sh <Server.json>
+# Usage: runLocally.sh <Server.json> [latest | <ServerRelease>]
 
 # Once started, you can test with:
 # 	http://localhost:8080/HealthCheck/
 
-# E.g., runLocally.sh ~/Desktop/Apps/SyncServerII/Private/Server/SharedImages-local.json
-# E.g., runLocally.sh ~/Desktop/Apps/SyncServerII/Private/Server/ClientTesting-local.json
+# E.g., runLocally.sh ~/Desktop/Apps/SyncServerII/Private/Server/SharedImages-local.json 0.19.1
+# E.g., runLocally.sh ~/Desktop/Apps/SyncServerII/Private/Server/ClientTesting-local.json latest
 SERVER_JSON=$1
+SERVER_RELEASE=$2
+
+if [ "empty$SERVER_RELEASE" != "empty" ]; then
+	SERVER_RELEASE=":${SERVER_RELEASE}"
+fi
+
 RUN_DIR=/Users/chris/Desktop/Apps/SyncServer.Run
 IMAGE=syncserver-runner
 
 # Copy Server.json file into the directory where the server's going to look for it
 cp "${SERVER_JSON}" "${RUN_DIR}"/Server.json
 
-docker run -p 8080:8080 --rm -i -t -v "${RUN_DIR}"/:/root/extras crspybits/syncserver-runner:latest
+docker run -p 8080:8080 --rm -i -t -v "${RUN_DIR}"/:/root/extras crspybits/"${IMAGE}${SERVER_RELEASE}"
