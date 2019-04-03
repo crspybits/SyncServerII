9489dac0f2d9151bd0f7fb8887a32aaf477d665c
Christopher Prince <chris@SpasticMuffin.biz>
2019-04-02 18:14:21 -0600

    Devops scripts + update package to fix issue.

---
+++ Migrations/9.sql
@@ -0,0 +1,62 @@
+-- Some of this script modified from https://stackoverflow.com/questions/6121917/automatic-rollback-if-commit-transaction-is-not-reached
+
+-- Local testing procedure-- to test migration
+-- 1) Export production db structure/contents from AWS RDS
+-- 2) Drop local db tables
+-- 3) Apply production AWS RDS dump to local db
+-- 4) Run migration SQL script on local db
+-- 5) Check tables and make sure structure looks right.
+
+-- Exporting from AWS RDS
+
+-- mysqldump -h url.for.db -P 3306 -u username -p --databases SyncServer_SharedImages > dump.sql
+
+-- use SyncServer_SharedImages;
+-- drop table DeviceUUID; drop table ShortLocks; drop table FileIndex; drop table MasterVersion; drop table SharingGroupUser; drop table Upload; drop table User; drop table SharingInvitation; drop table SharingGroup;
+
+-- importing to local db
+-- mysql -u root -p < ~/Desktop/dump.sql
+
+-- delete from DeviceUUID; delete from ShortLocks; delete from FileIndex; delete from MasterVersion; delete from SharingGroupUser; delete from Upload; delete from User; delete from SharingInvitation; delete from SharingGroup;
+
+-- show structure of a table
+-- describe Upload; -- or other table name
+
+delimiter //
+create procedure migration()
+begin 
+   DECLARE exit handler for sqlexception
+      BEGIN
+      
+      GET DIAGNOSTICS CONDITION 1 @p1 = RETURNED_SQLSTATE, @p2 = MESSAGE_TEXT;
+	  SELECT @p1, @p2, "ERROR999";
+     
+      ROLLBACK;
+   END;
+
+   DECLARE exit handler for sqlwarning
+     BEGIN
+      GET DIAGNOSTICS CONDITION 1 @p1 = RETURNED_SQLSTATE, @p2 = MESSAGE_TEXT;
+	  SELECT @p1, @p2, "ERROR999";
+	  
+     ROLLBACK;
+   END;
+   
+   START TRANSACTION;
+   		
+		ALTER TABLE SharingInvitation ADD COLUMN allowSocialAcceptance BOOL NOT NULL DEFAULT TRUE;
+      ALTER TABLE SharingInvitation ADD COLUMN numberAcceptors INT UNSIGNED NOT NULL DEFAULT 1;
+
+		SELECT "SUCCESS123";
+		
+   COMMIT;
+   
+end//
+delimiter ;
+
+-- Not needed because the command line invocation of mysql specifies the database.
+-- Use SyncServer_SharedImages;
+
+call migration();
+drop procedure migration;
+
---
+++ Migrations/migrate.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Usage:
+# 	arg1: the name of the .json configuration file for the server
+#	arg2: The name of the sql script.
+#	arg3: (optional) host-- replaces the host from the configuration file.
+# e.g., ./Migrations/migrate.sh ~/Desktop/Apps/SyncServerII/Private/Server/SharedImages-local.json Migrations/9.sql localhost
+# e.g., ./Migrations/migrate.sh ~/Desktop/Apps/SyncServerII/Private/Server.json.aws.app.bundles/Neebla-production.json Migrations/9.sql
+
+CONFIG_FILE=$1
+SQL_SCRIPT=$2
+CMD_LINE_HOST=$3
+
+DBNAME=`jq -r '.["mySQL.database"]' < ${CONFIG_FILE}`
+USER=`jq -r '.["mySQL.user"]' < ${CONFIG_FILE}`
+PASSWORD=`jq -r '.["mySQL.password"]' < ${CONFIG_FILE}`
+HOST=`jq -r '.["mySQL.host"]' < ${CONFIG_FILE}`
+
+if [ "empty$CMD_LINE_HOST" != "empty" ]; then
+	HOST=$CMD_LINE_HOST
+fi
+
+echo "Migrating $DBNAME on $HOST"
+
+result=$(mysql -P 3306 -p --user="$USER" --password="$PASSWORD" --host="$HOST" --database="$DBNAME" < "$SQL_SCRIPT" 2>&1 )
+
+# I've embedded "ERROR999" to be output from the sql script -- when an error and subsequent rollback occurs.
+
+ERROR=`echo $result | grep ERROR999`
+if [ "empty$ERROR" = "empty" ]; then
+	SUCCESS=`echo $result | grep SUCCESS123`
+	
+	# Didn't have ERROR string-- presumably no error.
+	
+	if [ "empty$SUCCESS" = "empty" ]; then
+		# Didn't find SUCCESS string-- must have error.
+		
+		echo "**** Failure running migration, despite no error marker *******"
+		echo $result
+	else
+		echo "Success running migration"
+	fi
+else
+    echo "**** Failure running migration (with error marker) *******"
+    echo $result
+fi
+
