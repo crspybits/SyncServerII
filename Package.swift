d970fe0d808ffc04f4d90a03ed4ba7b5dd4ef375
Christopher Prince <chris@SpasticMuffin.biz>
2019-04-08 08:51:04 -0600

    version 0.22.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 
         // .package(url: "../../repos/SyncServer-Shared", .branch("master")),
         // .package(url: "https://github.com/crspybits/SyncServer-Shared.git", .branch("dev")),
-        .package(url: "https://github.com/crspybits/SyncServer-Shared.git", from: "9.9.0"),
+        .package(url: "https://github.com/crspybits/SyncServer-Shared.git", from: "10.0.0"),
 
         // .package(url: "../../repos/SMServerLib", .branch("master")),
         .package(url: "https://github.com/crspybits/SMServerLib.git", from: "1.0.0"),
