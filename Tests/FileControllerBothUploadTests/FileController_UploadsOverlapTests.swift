ffe7ba5dd6e4398c47b2705a26843ed565b432ba
Christopher Prince <chris@SpasticMuffin.biz>
2020-08-05 21:18:27 -0600

    Working on upload deletions.

--- Tests/FileControllerBothUploadTests/FileController_UploadsOverlapTests.swift
+++ Tests/FileControllerBothUploadTests/FileController_UploadsOverlapTests.swift
@@ -60,7 +60,7 @@ class FileController_UploadsOverlapTests: ServerTestCase, UploaderCommon {
             return
         }
             
-        guard let _ = createUploadForTextFile(deviceUUID: deviceUUID, fileUUID: fileUUID1, sharingGroupUUID: sharingGroupUUID, userId: userId, deferredUploadId:deferredUploadId, updateContents: comment1.updateContents, uploadCount: 1, uploadIndex: 1, state: .vNUploadFileChange) else {
+        guard let _ = createUploadForTextFile(deviceUUID: deviceUUID, fileUUID: fileUUID1, fileGroupUUID: fileGroupUUID, sharingGroupUUID: sharingGroupUUID,  userId: userId, deferredUploadId:deferredUploadId, updateContents: comment1.updateContents, uploadCount: 1, uploadIndex: 1, state: .vNUploadFileChange) else {
             XCTFail()
             return
         }
@@ -166,5 +166,217 @@ class FileController_UploadsOverlapTests: ServerTestCase, UploaderCommon {
 
     func testUploadChangeWithNoPriorDeletionWorks() throws {
         try runUploadChange(withDeletionBefore: false)
+    }
+    
+    func runTwoUploadFileChangesAndThenOneUploadDeletionForSameFile(withFileGroup: Bool) throws {
+        let fileUUID1 = Foundation.UUID().uuidString
+        let deviceUUID = Foundation.UUID().uuidString
+        let changeResolverName = CommentFile.changeResolverName
+        
+        guard let deferredCount = DeferredUploadRepository(db).count() else {
+            XCTFail()
+            return
+        }
+        
+        guard let uploadCount = UploadRepository(db).count() else {
+            XCTFail()
+            return
+        }
+        
+        var fileGroupUUID: String?
+        if withFileGroup {
+            fileGroupUUID = Foundation.UUID().uuidString
+        }
+        
+        guard let result1 = uploadTextFile(uploadIndex: 1, uploadCount: 1, deviceUUID:deviceUUID, fileUUID: fileUUID1, stringFile: .commentFile, fileGroupUUID:fileGroupUUID, changeResolverName: changeResolverName),
+            let sharingGroupUUID = result1.sharingGroupUUID else {
+            XCTFail()
+            return
+        }
+        
+        guard let userId = result1.uploadingUserId else {
+            XCTFail()
+            return
+        }
+        
+        let comment1 = ExampleComment(messageString: "Example", id: Foundation.UUID().uuidString)
+        let comment2 = ExampleComment(messageString: "Example", id: Foundation.UUID().uuidString)
+        
+        guard let deferredUpload1 = createDeferredUpload(fileGroupUUID: fileGroupUUID, sharingGroupUUID: sharingGroupUUID, status: .pendingChange),
+            let deferredUploadId1 = deferredUpload1.deferredUploadId else {
+            XCTFail()
+            return
+        }
+            
+        guard let _ = createUploadForTextFile(deviceUUID: deviceUUID, fileUUID: fileUUID1, fileGroupUUID: fileGroupUUID, sharingGroupUUID: sharingGroupUUID, userId: userId, deferredUploadId:deferredUploadId1, updateContents: comment1.updateContents, uploadCount: 1, uploadIndex: 1, state: .vNUploadFileChange) else {
+            XCTFail()
+            return
+        }
+        
+        guard let deferredUpload2 = createDeferredUpload(fileGroupUUID: fileGroupUUID, sharingGroupUUID: sharingGroupUUID, status: .pendingChange),
+            let deferredUploadId2 = deferredUpload2.deferredUploadId else {
+            XCTFail()
+            return
+        }
+            
+        guard let _ = createUploadForTextFile(deviceUUID: deviceUUID, fileUUID: fileUUID1, fileGroupUUID: fileGroupUUID, sharingGroupUUID: sharingGroupUUID, userId: userId, deferredUploadId:deferredUploadId2, updateContents: comment2.updateContents, uploadCount: 1, uploadIndex: 1, state: .vNUploadFileChange) else {
+            XCTFail()
+            return
+        }
+        
+        let uploadDeletionRequest = UploadDeletionRequest()
+        uploadDeletionRequest.sharingGroupUUID = sharingGroupUUID
+        if withFileGroup {
+            uploadDeletionRequest.fileGroupUUID = fileGroupUUID
+        }
+        else {
+            uploadDeletionRequest.fileUUID = fileUUID1
+        }
+        
+        guard let _ = uploadDeletion(uploadDeletionRequest: uploadDeletionRequest, deviceUUID: deviceUUID, addUser: false) else {
+            XCTFail()
+            return
+        }
+        
+        guard let fileIndex1 = getFileIndex(sharingGroupUUID: sharingGroupUUID, fileUUID: fileUUID1) else {
+            XCTFail()
+            return
+        }
+        
+        // Expectation: File should be deleted.
+        let found1 = try fileIsInCloudStorage(fileIndex: fileIndex1)
+        XCTAssert(!found1)
+        
+        XCTAssert(deferredCount == DeferredUploadRepository(db).count())
+        XCTAssert(uploadCount == UploadRepository(db).count(), "\(uploadCount) != \(String(describing: UploadRepository(db).count())))")
+    }
+    
+    func testRunTwoUploadFileChangesAndThenOneUploadDeletionForSameFileWithFileGroup() throws {
+        try runTwoUploadFileChangesAndThenOneUploadDeletionForSameFile(withFileGroup: true)
+    }
+    
+    func testRunTwoUploadFileChangesAndThenOneUploadDeletionForSameFileWithoutFileGroup() throws {
+        try runTwoUploadFileChangesAndThenOneUploadDeletionForSameFile(withFileGroup: false)
+    }
+    
+    func runTwoChangesAndThenOneDeletionForSameFilePlusOtherUploadChange(withFileGroup: Bool) throws {
+        let fileUUID1 = Foundation.UUID().uuidString
+        let fileUUID2 = Foundation.UUID().uuidString
+        let deviceUUID = Foundation.UUID().uuidString
+        let changeResolverName = CommentFile.changeResolverName
+        
+        guard let deferredCount = DeferredUploadRepository(db).count() else {
+            XCTFail()
+            return
+        }
+        
+        guard let uploadCount = UploadRepository(db).count() else {
+            XCTFail()
+            return
+        }
+        
+        var fileGroupUUID: String?
+        if withFileGroup {
+            fileGroupUUID = Foundation.UUID().uuidString
+        }
+        
+        guard let result1 = uploadTextFile(uploadIndex: 1, uploadCount: 1, deviceUUID:deviceUUID, fileUUID: fileUUID1, stringFile: .commentFile, fileGroupUUID:fileGroupUUID, changeResolverName: changeResolverName),
+            let sharingGroupUUID = result1.sharingGroupUUID else {
+            XCTFail()
+            return
+        }
+        
+        // Explicitly not including this one in file group because we're going to upload a change for it, which we don't want removed with the other file group.
+        guard let _ = uploadTextFile(uploadIndex: 1, uploadCount: 1, deviceUUID:deviceUUID, fileUUID: fileUUID2, addUser: .no(sharingGroupUUID: sharingGroupUUID), stringFile: .commentFile, changeResolverName: changeResolverName) else {
+            XCTFail()
+            return
+        }
+        
+        guard let userId = result1.uploadingUserId else {
+            XCTFail()
+            return
+        }
+        
+        let comment1 = ExampleComment(messageString: "Example", id: Foundation.UUID().uuidString)
+        let comment2 = ExampleComment(messageString: "Example", id: Foundation.UUID().uuidString)
+        let comment3 = ExampleComment(messageString: "Example", id: Foundation.UUID().uuidString)
+        
+        guard let deferredUpload1 = createDeferredUpload(fileGroupUUID: fileGroupUUID, sharingGroupUUID: sharingGroupUUID, status: .pendingChange),
+            let deferredUploadId1 = deferredUpload1.deferredUploadId else {
+            XCTFail()
+            return
+        }
+            
+        guard let _ = createUploadForTextFile(deviceUUID: deviceUUID, fileUUID: fileUUID1, fileGroupUUID: fileGroupUUID, sharingGroupUUID: sharingGroupUUID, userId: userId, deferredUploadId:deferredUploadId1, updateContents: comment1.updateContents, uploadCount: 1, uploadIndex: 1, state: .vNUploadFileChange) else {
+            XCTFail()
+            return
+        }
+        
+        guard let deferredUpload2 = createDeferredUpload(fileGroupUUID: fileGroupUUID, sharingGroupUUID: sharingGroupUUID, status: .pendingChange),
+            let deferredUploadId2 = deferredUpload2.deferredUploadId else {
+            XCTFail()
+            return
+        }
+            
+        guard let _ = createUploadForTextFile(deviceUUID: deviceUUID, fileUUID: fileUUID1, fileGroupUUID: fileGroupUUID, sharingGroupUUID: sharingGroupUUID, userId: userId, deferredUploadId:deferredUploadId2, updateContents: comment2.updateContents, uploadCount: 1, uploadIndex: 1, state: .vNUploadFileChange) else {
+            XCTFail()
+            return
+        }
+        
+        // Upload change for other file-- expecting this to *not* be pruned
+        guard let deferredUpload3 = createDeferredUpload(sharingGroupUUID: sharingGroupUUID, status: .pendingChange),
+            let deferredUploadId3 = deferredUpload3.deferredUploadId else {
+            XCTFail()
+            return
+        }
+            
+        guard let upload3 = createUploadForTextFile(deviceUUID: deviceUUID, fileUUID: fileUUID2, sharingGroupUUID: sharingGroupUUID, userId: userId, deferredUploadId:deferredUploadId3, updateContents: comment3.updateContents, uploadCount: 1, uploadIndex: 1, state: .vNUploadFileChange) else {
+            XCTFail()
+            return
+        }
+        
+        
+        
+        let uploadDeletionRequest = UploadDeletionRequest()
+        uploadDeletionRequest.sharingGroupUUID = sharingGroupUUID
+        if withFileGroup {
+            uploadDeletionRequest.fileGroupUUID = fileGroupUUID
+        }
+        else {
+            uploadDeletionRequest.fileUUID = fileUUID1
+        }
+        
+        guard let _ = uploadDeletion(uploadDeletionRequest: uploadDeletionRequest, deviceUUID: deviceUUID, addUser: false) else {
+            XCTFail()
+            return
+        }
+        
+        guard let fileIndex1 = getFileIndex(sharingGroupUUID: sharingGroupUUID, fileUUID: fileUUID1) else {
+            XCTFail()
+            return
+        }
+        
+        guard let fileIndex2 = getFileIndex(sharingGroupUUID: sharingGroupUUID, fileUUID: fileUUID2) else {
+            XCTFail()
+            return
+        }
+        
+        // Expectation: File should be deleted.
+        let found1 = try fileIsInCloudStorage(fileIndex: fileIndex1)
+        XCTAssert(!found1)
+
+        let found2 = try fileIsInCloudStorage(fileIndex: fileIndex2)
+        XCTAssert(found2)
+        
+        XCTAssert(deferredCount == DeferredUploadRepository(db).count())
+        XCTAssert(uploadCount == UploadRepository(db).count(), "\(uploadCount) != \(String(describing: UploadRepository(db).count())))")
+    }
+    
+    func testRunTwoChangesAndThenOneDeletionForSameFilePlusOtherUploadChangeWithFileGroup() throws {
+        try runTwoChangesAndThenOneDeletionForSameFilePlusOtherUploadChange(withFileGroup: true)
+    }
+    
+    func testRunTwoChangesAndThenOneDeletionForSameFilePlusOtherUploadChangeWithoutFileGroup() throws {
+        try runTwoChangesAndThenOneDeletionForSameFilePlusOtherUploadChange(withFileGroup: false)
     }
 }
