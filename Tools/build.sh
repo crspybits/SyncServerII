65c08120e53bd481930612cf82680799b79b1cba
Christopher Prince <chris@SpasticMuffin.biz>
2021-01-10 17:07:47 -0700

    Added comment and updated CredentialsAppleSignIn version.

--- Tools/build.sh
+++ Tools/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+## Note: I'd like to have the option of doing `swift package update` as part of this, but it seems like while you can run `swift build --skip-update`, there's no `swift build --update`. And running `swift build` by itself does *not* update from the latest packages. I just verified this. Odd.
+## In order to update, you have to do this in two steps. First, run `swift package update` and then second, run this build script. This seems to do more work than it needs to. It actually seems to update twice this way.
+
 # Usage: build [verbose]
+
 VERBOSE=""
 if [ "$1empty" == "verboseempty" ]; then
 	VERBOSE="-v"
